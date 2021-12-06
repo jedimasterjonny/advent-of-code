7b81a7b0e80fc5c53fe6b030ea465bd53abe8445
jedimasterjonny <46541898+jedimasterjonny@users.noreply.github.com>
2021-12-06 10:21:35 +0000

    add missing part five

---
+++ 5/part-ab.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+
+def go(is_b=False):
+    with open("in") as f:
+        inp = [list(map(int, line.strip().replace("->", ",").split(","))) for line in f]
+
+    field = [
+        [0 for _ in range(max(max([(c[0], c[2]) for c in inp])) + 1)]
+        for _ in range(max(max([(c[1], c[3]) for c in inp])) + 1)
+    ]
+
+    for x1, y1, x2, y2 in inp:
+        xmin, dx = min(x1, x2), abs(x2 - x1)
+        ymin, dy = min(y1, y2), abs(y2 - y1)
+        if x1 == x2:
+            for i in range(dy + 1):
+                field[i + ymin][x1] += 1
+        elif y1 == y2:
+            for i in range(dx + 1):
+                field[y1][i + xmin] += 1
+        elif is_b:
+            cx = 1 if x2 > x1 else -1
+            cy = 1 if y2 > y1 else -1
+            for i in range(dx + 1):
+                field[cy * i + y1][cx * i + x1] += 1
+
+    return sum([1 for line in field for num in line if num > 1])
+
+
+print("part a:", go())
+print("part b:", go(True))
